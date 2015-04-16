043d50fd658c09a2ec70e33d17a54c54031a66f5
Jessalyn Cheng <jyling@gmail.com>
2015-04-16 12:19:58 -0700

    Lab 1
    Started Assignment 1

---
+++ assignment1RD.py
@@ -0,0 +1,10 @@
+# Title: Homework 1: Data Scraping
+
+# Description: This is our first Data Science Homework, where we need to scrape data from a website to find 300 unique faculty names. 
+
+# Notes  
+# 1. 4/16: I'm working on installing python and beautiful soup today in the lab
+
+# Author:  Jessalyn Cheng
+# Date:  April 16, 2015
+
